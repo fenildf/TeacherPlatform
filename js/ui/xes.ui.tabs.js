465b6a6c3b99a347f681057bd592a3ff77a4cd75
riasky <riasky@msn.com>
2012-11-01 21:13:10 +0800

    add ui.tabs

--- js/ui/xes.ui.tabs.js
+++ js/ui/xes.ui.tabs.js
@@ -16,38 +16,110 @@ var tabs = tabs || {};
 (function(x, undefined){
 	var t = tabs;
 	t.id = 0;
+	/*
+	 * tab列表数据
+	 *  id 		: 标签id
+		title	: 标签文字
+		content	: 标签对应的内容
+		url 	: 标签目标url地址，存在的话，则用iframe调用
+		fixed	: 固定选项：true（不可以关闭），默认是false
+	 *
+	 **/
+	t.data = [
+		{ 'id': '01', 'title': '首页', 'content': '', 'url': false, 'fixed': true },
+		{ 'id': '02', 'title': 'menu1', 'content': '', 'url': false, 'fixed': true },
+		{ 'id': '03', 'title': 'menu2', 'content': '', 'url': false, 'fixed': true },
+		{ 'id': '04', 'title': 'menu3', 'content': '', 'url': false, 'fixed': false },
+		{ 'id': '05', 'title': 'menu3', 'content': '', 'url': false, 'fixed': false },
+		{ 'id': '06', 'title': 'menu4', 'content': '', 'url': false, 'fixed': false },
+		{ 'id': '07', 'title': 'menu5', 'content': '', 'url': false, 'fixed': false },
+		{ 'id': '08', 'title': 'menu6', 'content': '', 'url': false, 'fixed': false },
 
-	xes.ui.add('tabs',{
-		version: '1.0',
-		options: {
-			active : null,
-			event : 'click',
-			load : null,
-			before : null
-		},
+		{ 'id': '09', 'title': 'menu7', 'content': '', 'url': false, 'fixed': false }
+	];
+
+	//相关元素的cls，方便jQuery检索
+	t.cls = {
+		wrap : '.ui-tabs-wrap',
+		last : '.ui-tabs-last',
+		fixed : '.fixed',
+		active : '.current'
+	};
+
+	//返回tabs的html结构
+	t.getHtml = function(d){
+		var _tpl = {
+			target : d.url ? 'target="_content"' : '',
+			url : d.url ? d.url : 'javascript:void(0);',
+			title : d.title,
+			close : d.fixed ? '' : '<span class="del_btn">删除</span>'
+		};
+		var _html = '<li><a "' + _tpl.target +'" href="' + _tpl.url + '">' + _tpl.title + '</a>' + _tpl.close + '</li>';
+		return _html;
+	};
+
+	//监听事件
+	t.listener = function(dom, event, fn){
+		dom[event](function(){
+			fn();
+		});
+	};
+
+	/* 
+	 * 创建tab元素 
+	 * d = {title,};
+	 */
+	t.create = function(html){
+
+	};
+
+	/*
+	 * 
+	 */
+	t.handleClick = function(){
+
+	};
+
+
+
+
+	// t = {
+	// 	version: '1.0',
+	// 	options: {
+	// 		active : null,
+	// 		event : 'click',
+	// 		load : null,
+	// 		before : null
+	// 	},
 		
-		_setOption  : function( event ){},
-		_create     : function(){},
-		_url        : function( index, url ){},
-		_length     : function(){},
-		_tabId      : function( a ){},
-		_getIndex   : function( index ){},
-		_findActive : function( selector ){},
-		_toggle     : function( event, eventData ){},
+	// 	_setOption  : function( event ){},
+	// 	_create     : function(){},
+	// 	_url        : function( index, url ){},
+	// 	_length     : function(){},
+	// 	_tabId      : function( a ){},
+	// 	_getIndex   : function( index ){},
+	// 	_findActive : function( selector ){},
+	// 	_toggle     : function( event, eventData ){},
 
-		enable  : function( index ){},
-		disable : function( index ){},
+	// 	enable  : function( index ){},
+	// 	disable : function( index ){},
 
-		add     : function( url, label, index ){},
-		remove  : function( index ){},
+	// 	add     : function( url, label, index ){},
+	// 	remove  : function( index ){},
 		
-		load    : function( index, event ){},
+	// 	load    : function( index, event ){},
 		
-		refresh : function(){},
+	// 	refresh : function(){},
 
-		select  : function( index ){},
-		
+	// 	select  : function( index ){},
 
-	});
+	// };
 
 })(xes);
+
+
+
+
+xes.ui.add( 'tabs', tabs , function(tips){
+
+});
