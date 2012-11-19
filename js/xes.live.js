726d5431abd54e061b4461aeb8d9d70a82007cd6
riasky <riasky@msn.com>
2012-11-19 15:55:13 +0800

    增加日期插件

---
+++ js/xes.live.js
@@ -0,0 +1,24 @@
+/*
+ * XESUI
+ * Copyright 2012 xueersi.com All rights reserved.
+ */
+
+/*
+ * 直播
+ * @update : 2012-10-05
+ * @author : Marco <Marco.Pai@msn.com>
+ * @version: v1.0.0
+ */
+
+var xes = xes || {};
+
+xes.live = xes.live || {};
+
+(function(){
+	var live = xes.live;
+
+	live.timeChoose = function(){};
+
+
+
+})();
